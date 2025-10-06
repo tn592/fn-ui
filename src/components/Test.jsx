1c1f283416266031d90382ce13345fb4f1a57026
tanzina <tn59249@yahoo.com>
2025-10-06 16:09:07 +0600

    first commit

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+const Test = () => {
+	return (
+		<div>
+			<h1 className="text-3xl font-semibold text-green-500">
+				This is the starting of the frontend project!!!
+			</h1>
+		</div>
+	);
+};
+
+export default Test;
